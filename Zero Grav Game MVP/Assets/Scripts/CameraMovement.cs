b738a35bd9e7f145efd2613d13f8f10d4ed81990
vab10266 <vaudalexburton@gmail.com>
2019-06-20 16:44:42 -0400

    Previous Files

---
+++ Zero Grav Game MVP/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+	public float turnSpeed = 100.0f;
+
+
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+        if(Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+
+		if(Input.GetKey(KeyCode.UpArrow))
+            transform.Rotate(Vector3.left, turnSpeed * Time.deltaTime);
+
+        if(Input.GetKey(KeyCode.DownArrow))
+            transform.Rotate(Vector3.left, -turnSpeed * Time.deltaTime);
+	}
+}
